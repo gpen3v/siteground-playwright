e7b0abc785e292ca3a64ae9b419bbea22e4bde20
Georgi Penev <georgipenev@Georgis-MacBook-Pro.local>
2026-01-20 18:31:00 +0200

    Documentation comments, custom expect messages, improved dropdown method

--- pages/emailPage.ts
+++ pages/emailPage.ts
@@ -3,8 +3,11 @@ import { domains } from '../test-data/testData.json'
 
 export class EmailPage {
   readonly page: Page;
+  // Common locators
   readonly domainDropdown: Locator;
-
+  readonly domainDropdownInput: Locator;
+  readonly domainDropdownOption: Locator;
+  // Email Accounts locators
   readonly accountNameField: Locator;
   readonly passwordComponent: Locator;
   readonly passwordField: Locator;
@@ -12,14 +15,17 @@ export class EmailPage {
   readonly createButton: Locator;
   readonly notificationMessage: Locator;
   readonly accountNameTableCell: Locator;
-
+  // Email Forwarders locators
   readonly forwarderNameField: Locator;
   readonly forwarderNameFieldValidation: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    // Common locators
     this.domainDropdown = page.getByTestId('domain-select');
-
+    this.domainDropdownInput = this.domainDropdown.locator('input');
+    this.domainDropdownOption = this.domainDropdown.getByRole('option');
+    // Email Accounts locators
     this.accountNameField = page.getByTestId('text-input-name');
     this.passwordComponent = page.getByTestId('form-password-password-label');
     this.passwordField = this.passwordComponent.getByTestId('form-password-password');
@@ -27,18 +33,32 @@ export class EmailPage {
     this.createButton = page.getByTestId('create-box-submit');
     this.notificationMessage = page.getByTestId('box-notification').locator('h3');
     this.accountNameTableCell = page.getByTestId('table-cell').locator('p');
-
+    // Email Forwarders locators
     this.forwarderNameField = page.getByTestId('forward-crate-name-label');
     this.forwarderNameFieldValidation = this.forwarderNameField.getByTestId('validation');
   }
 
-  async dropdownValidateAndSelect(dropdown: Locator, option: string) {
-    await dropdown.click();
-    const options = dropdown.getByRole('option');
-    await expect(options).toHaveCount(domains.length);
+  /**
+   * Clicks on the domain dropdown, validates available options against test data,
+   * selects the specified domain, and verifies it is selected.
+   * @param option - The domain value to select from the dropdown.
+   */
+  async domainDropdownValidateAndSelect(option: string) {
+    await this.domainDropdown.click();
+    const options = this.domainDropdownOption;
+    await expect(
+      options,
+      `${await options.count()} options in the Select Domain dropdown, ${domains.length} domains in testData.json`
+    ).toHaveCount(domains.length);
     const optionTexts = await options.allTextContents();
-    expect(optionTexts).toEqual(domains);
-    await dropdown.getByRole('option').getByText(option).click();
-    await expect(dropdown.locator('input')).toHaveAttribute('placeholder', option);
+    expect(
+      optionTexts,
+      `Domain options: ${optionTexts}`
+    ).toEqual(domains);
+    await this.domainDropdownOption.getByText(option).click();
+    await expect(
+      this.domainDropdownInput,
+      `${option} is selected in the Select Domain dropdown`
+    ).toHaveAttribute('placeholder', option);
   }
 }
--- tests/emailSection.spec.ts
+++ tests/emailSection.spec.ts
@@ -19,36 +19,63 @@ test.describe('SiteGround Web Application - Email Section tests', () => {
     // Go to the web application
     await page.goto('/');
     const firstName = await getFirstName(page);
-    await expect(page.locator('h1')).toHaveText(`Hello, ${firstName}!`);
+    await expect(
+      page.locator('h1'),
+      `Hello, ${firstName}! title is displayed`
+    ).toHaveText(`Hello, ${firstName}!`);
   });
 
   test('Add a New Email Account', async ({ page }) => {
     
+    // Navigate to Email Accounts
     await navigation.emailNavigationGroup.click();
     await navigation.emailLink.click();
-    await expect(page.locator('h1')).toHaveText(testData.pageTitles.accounts);
+    await expect(
+      page.locator('h1'),
+      `Page heading: ${testData.pageHeadings.accounts}`
+    ).toHaveText(testData.pageHeadings.accounts);
 
-    await emailSection.dropdownValidateAndSelect(emailSection.domainDropdown, domain);
+    // Validate Select Domain dropdown and select a domain
+    await emailSection.domainDropdownValidateAndSelect(domain);
     
+    // Create an account with account name and generated password
     await emailSection.accountNameField.fill(`${testAccount}`);
     await emailSection.passwordGenerate.click();
-    await expect(emailSection.passwordField).not.toHaveValue('');
-
+    await expect(
+      emailSection.passwordField,
+      `Password is generated`
+    ).not.toHaveValue('');
     await emailSection.createButton.click();
-    await expect(emailSection.notificationMessage).toHaveText(`Email account ${testAccount}@${domain} is created.`)
-
-    await expect(emailSection.accountNameTableCell.getByText(`${testAccount}@${domain}`)).toBeVisible();
+    await expect(
+      emailSection.notificationMessage,
+      `Successful message: Email account ${testAccount}@${domain} is created.`
+    ).toHaveText(`Email account ${testAccount}@${domain} is created.`)
+    
+    // Validate the new account in the Manage Email Accounts table
+    await expect(
+      emailSection.accountNameTableCell.getByText(`${testAccount}@${domain}`),
+      `${testAccount}@${domain} account appears in the “Manage Email Accounts table`
+    ).toBeVisible();
   });
 
   test('Add a New Empty Email Forwarder', async ({ page }) => {
   
+    // Navigate to Email Forwarders
     await navigation.emailNavigationGroup.click();
     await navigation.forwardersLink.click();
-    await expect(page.locator('h1')).toHaveText(testData.pageTitles.forwarders);
-  
-    await emailSection.dropdownValidateAndSelect(emailSection.domainDropdown, domain);
-  
+    await expect(
+      page.locator('h1'),
+      `Page heading: ${testData.pageHeadings.forwarders}`
+    ).toHaveText(testData.pageHeadings.forwarders);
+
+    // Validate Select Domain dropdown and select a domain
+    await emailSection.domainDropdownValidateAndSelect(domain);
+    
+    // Validate the "Required field." error message for "Forward all messages sent to:” input
     await emailSection.createButton.click();
-    await expect(emailSection.forwarderNameFieldValidation).toHaveText(testData.validationRequiredField);
+    await expect(
+      emailSection.forwarderNameFieldValidation,
+      `${testData.validationRequiredField} error message for`
+    ).toHaveText(testData.validationRequiredField);
   });
 });
